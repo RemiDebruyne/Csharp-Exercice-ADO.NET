0177bf55ff926d23c5a301235b681eee9e71c818
Remi Debruyne <remidebruyne@hotmail.com>
2024-01-15 14:25:56 +0100

    first commit

---
+++ Demo01/Program.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Core;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+// connectionString
+string conncetionString = "Data Source=(localdb)\\exerciceDB;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False; Database = contactDB";
+
+// Création d'un objet SQLConnection présent dans la libraire ADO.NET
+SqlConnection connection = new SqlConnection(conncetionString);
+
+connection.Open();
+
+if(connection.State == ConnectionState.Open)
+    Console.WriteLine("La connexion est ouverte");
+else
+    Console.WriteLine("Je suis très triste");
+
+// Requête
+
+string prenom = Console.ReadLine() ?? "";
+string nom = Console.ReadLine() ?? "";
+string email = Console.ReadLine() ?? "";
+
+string req = $"INSERT INTO personnes (prenom, nom, email) VALUES ('{prenom}','{nom}', '{email}')";
+
+// On instancie un objet de commande qui contient la requete, la connexion ainsi que la transaction si besoin
+SqlCommand command = new SqlCommand(req, connection);
+
+// ExecuteNonQuery exécute la requete et renvoie le nombre d'élément affectés
+int rowsAffected = command.ExecuteNonQuery();
+
+Console.WriteLine(rowsAffected);
+
+command.Dispose();
+
+connection.Close();
---
+++ SQLQuery1.sql
@@ -0,0 +1,20 @@
+﻿-- on se positionne sur la base données
+Use contactDB;
+GO
+
+-- Création de la table personnes
+CREATE TABLE personnes (
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	prenom VARCHAR(50) NOT NULL,
+	nom VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL
+);
+GO
+
+INSERT INTO 
+	personnes (prenom, nom, email)
+VALUES
+	('emanuelle','Macron','manumicron@gmail.com');
+GO
+
+SELECT * FROM personnes;
