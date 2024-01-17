5de7cae23bd9e5057829fea4d79ef8005d27cbf4
remiD <remidebruyne@hotmail.com>
2024-01-17 13:47:39 +0100

    build: add basic structure

---
+++ ExerciceCommande/DAO/BaseDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceCommande.DAO
+{
+    internal abstract class BaseDAO<T>
+    {
+        protected string request = "";
+
+        public abstract List<T> GetAll();
+        public abstract T? GetOneById(int id);
+        public abstract T Save(T element);
+        public abstract T Update(T element);
+        public abstract T Delte(T element);
+        public abstract T Add(T element);
+    }
+}
---
+++ ExerciceCommande/database.sql
@@ -0,0 +1,24 @@
+﻿CREATE DATABASE exerciceCommandes;
+GO
+
+CREATE TABLE [dbo].[clients]
+(
+	[id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[prenom] VARCHAR(50) NOT NULL,
+	[nom] VARCHAR(50) NOT NULL,
+	[adresse] VARCHAR(50) NOT NULL,
+	[cp] VARCHAR(50) NOT NULL,
+	[ville] VARCHAR(50) NOT NULL,
+	[telephone] VARCHAR(50) NOT NULL,
+);
+GO
+
+CREATE TABLE [dbo].[commandes]
+(
+	[id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[client_id] INT, 
+	[date] DATETIME NOT NULL,
+	[total] DEC,
+	CONSTRAINT FK_commande_client_id FOREIGN KEY([client_id]) REFERENCES clients(id)
+);
+GO
